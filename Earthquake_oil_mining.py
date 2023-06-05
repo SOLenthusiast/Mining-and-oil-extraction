4612763331d49c4945d3258dde5e64b544557282
Sol_enthusiast <101778567+SOLenthusiast@users.noreply.github.com>
2023-06-05 11:30:26 -0400

    Add files via upload

---
+++ Earthquake_oil_mining.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pyplot.matplotlib as plt
+import seaborn as sns
+import pandas ad pd
+
+## Investigate the effect that increased injection of saline wastewater due to oil mining in Oklahoma has had on the seismicity of the region.
+# Plot time vs. magnitude
+plt.plot(time, mags, marker='.', linestyle='none', alpha=0.1)
+
+# Label axes and show the plot
+plt.xlabel('magnitude')
+plt.ylabel('time (year)')
+plt.show()
+
+# Compute mean interearthquake time
+mean_dt_pre = np.mean(dt_pre)
+mean_dt_post = np.mean(dt_post)
+
+# Draw 10,000 bootstrap replicates of the mean
+bs_reps_pre = dcst.draw_bs_reps(dt_pre, np.mean, size=10000)
+bs_reps_post = dcst.draw_bs_reps(dt_post, np.mean, size=10000)
+
+# Compute the confidence interval
+conf_int_pre = np.percentile(bs_reps_pre, [2.5, 97.5])
+conf_int_post = np.percentile(bs_reps_post, [2.5, 97.5])
+
+# Print the results
+print("""1980 through 2009
+mean time gap: {0:.2f} days
+95% conf int: [{1:.2f}, {2:.2f}] days""".format(mean_dt_pre, *conf_int_pre))
+
+
+# Print the results
+print("""1980 through 2009
+mean time gap: {0:.2f} days
+95% conf int: [{1:.2f}, {2:.2f}] days""".format(mean_dt_pre, *conf_int_pre))
+
+print("""
+2010 through mid-2017
+mean time gap: {0:.2f} days
+95% conf int: [{1:.2f}, {2:.2f}] days""".format(mean_dt_post, *conf_int_post))
+
+# Compute the observed test statistic
+mean_dt_diff = mean_dt_pre - mean_dt_post
+
+# Shift the post-2010 data to have the same mean as the pre-2010 data
+dt_post_shift = dt_post - mean_dt_post + mean_dt_pre
+
+# Compute 10,000 bootstrap replicates from arrays
+bs_reps_pre = dcst.draw_bs_reps(dt_pre, np.mean, size=10000)
+bs_reps_post = dcst.draw_bs_reps(dt_post_shift, np.mean, size=10000)
+
+# Get replicates of difference of means
+bs_reps = bs_reps_pre - bs_reps_post
+
+# Compute and print the p-value
+p_val = np.sum(bs_reps >= mean_dt_diff) / 10000
+print('p =', p_val)
+
+# Get magnitudes before and after 2010
+mags_pre = mags[time < 2010]
+mags_post = mags[time >= 2010]
+
+# Generate ECDFs
+_ = plt.plot(*dcst.ecdf(mags_pre), marker='.', linestyle='none')
+_ = plt.plot(*dcst.ecdf(mags_post), marker='.', linestyle='none')
+
+# Label axes and show plot
+_ = plt.xlabel('magnitude')
+_ = plt.ylabel('ECDF')
+plt.legend(('1980 though 2009', '2010 through mid-2017'), loc='upper left')
+plt.show()
+
+# Compute b-value and confidence interval for pre-2010
+b_pre, conf_int_pre = b_value(mags_pre, mt, perc=[2.5, 97.5], n_reps=10000)
+
+# Compute b-value and confidence interval for post-2010
+b_post, conf_int_post = b_value(mags_post, mt, perc=[2.5, 97.5], n_reps=10000)
+
+# Report the results
+print("""
+1980 through 2009
+b-value: {0:.2f}
+95% conf int: [{1:.2f}, {2:.2f}]
+
+2010 through mid-2017
+b-value: {3:.2f}
+95% conf int: [{4:.2f}, {5:.2f}]
+""".format(b_pre, *conf_int_pre, b_post, *conf_int_post))
+
+# Only magnitudes above completeness threshold
+mags_pre = mags_pre[mags_pre >= mt ]
+mags_post = mags_post[mags_post >= mt]
+
+# Observed difference in mean magnitudes: diff_obs
+diff_obs = np.mean(mags_post) - np.mean(mags_pre)
+
+# Generate permutation replicates: perm_reps
+perm_reps = dcst.draw_perm_reps(mags_post, mags_pre, dcst.diff_of_means, size=10000)
+
+# Compute and print p-value
+p_val = np.sum(perm_reps < diff_obs) / 10000
+print('p =', p_val)
